eb5dc2abe843d189ddc199efa6ee465b2543a1a1
TackWooLee-Leon <155127100+TackWooLee-Leon@users.noreply.github.com>
2024-08-18 15:50:05 -0400

    Create pokemon storage componen
    Add 2 react Select components for storage
    
    Update dup function, edit selectedPokemon state structure
    
    Fix functions

--- src/Components/PokemonStorage.js
+++ src/Components/PokemonStorage.js
@@ -1,54 +1,196 @@
-// import styles from './Table.module.css'
+import styles from './Table.module.css'
+import React, { useState, useRef } from 'react'
+import Select from 'react-select';
 
-// export default function PokemonStorage () {
 
-//     <td className={styles.pokemonDisplay}> 
-//         <div className={styles.popUpMenu} style={{
-//             display: showPopUp[1] ? "flex" : "none", 
-//             position: "absolute", 
-//             bottom: "65px", 
-//             left: "-40px"
-//         }}>
-//             <div className={styles.selectWrapper}>
-//                 <Select 
-//                     placeholder="Find Pokemon"
-//                     onChange={(selectedOption) => {handleSelectChange(player2Index, selectedOption)}}
-//                     formatOptionLabel={(pokemon) => {
-//                         return ( 
-//                             <div style={{ display: 'flex', alignItems: 'center'}}>
-//                                 <img src={pokemon.spriteUrl} alt={pokemon.name} style={{ width: 50, marginRight: 5 }}></img>
-//                                 <span>{pokemon.name}</span>
-//                             </div>)
-//                     }}
-//                     options={pokemonInfo} 
-//                     getOptionLabel={options => options.name}
-//                     getOptionValue={options => options.name}
-//                     value={selectedPokemon[player2Index] && pokemonInfo.find(pokemon => pokemon.name === selectedPokemon[player2Index].name)}
-//                     />
-//             </div>
-//             <input 
-//                 type="text" 
-//                 placeholder='Enter nickname'
-//                 ref={input2Ref}
-//             />
+export default function PokemonStorage ({ playerProps, selectedProps, pokemonInfo, setSelectedPokemon }) {
+    const { player1Index, player2Index, selectedPokemon, buttonBackgroundImage } = playerProps;
+    const [showPopUp, setShowPopUp] = useState(Array(2).fill(false));
+    const [storagePlayer2Nickname, setStoragePlayer2Nickname] = useState("")
 
-//             <div className={styles.popUpMenuBtns}>
-//                 <button onClick={() => {togglePopUp(1)}}>Cancel</button>
-//                 <button 
-//                 onClick={() => {togglePopUp(1); displaySecondName(); handleAddButtonClick(player2Index); checkForDuplicatingTypes()}}>Add</button>
-//             </div>
-//         </div>
+    function togglePopUp(index) {
+        setShowPopUp((prevShowPopUp) => {
+            const newShowPopUp = [...prevShowPopUp];
+            newShowPopUp[index] = !newShowPopUp[index];
+            return newShowPopUp
+        });
+    }
 
-//         <button style={{  
-//             backgroundImage: `url(${buttonBackgroundImage[player2Index]})`,
-//             backgroundSize: 'cover',
-//             backgroundRepeat: 'no-repeat',
-//             height: "4rem", 
-//             width: "4rem",
-//             borderRadius: '15px',
-//             border: 'none',
-//         }}
-//             onClick={() => {togglePopUp(1)}}>
-//         +</button>
-//     </td>
-// }
+    // const [storageSelectedPokemon, setStorageSelectedPokemon] = useState({
+    //     0: {name: '', pokemonTypes: '', spriteUrl: ''},
+    //     1: {name: '', pokemonTypes: '', spriteUrl: ''},
+    //     2: {name: '', pokemonTypes: '', spriteUrl: ''},
+    //     3: {name: '', pokemonTypes: '', spriteUrl: ''},
+    //     4: {name: '', pokemonTypes: '', spriteUrl: ''},
+    //     5: {name: '', pokemonTypes: '', spriteUrl: ''},
+    // });
+
+    const storageHandleSelectChange = (optionIndex, selectedOption) => {
+        const { name, pokemonTypes, spriteUrl } = selectedOption;
+        setSelectedPokemon(prevState => ({
+            ...prevState,
+            storage: prevState.storage.map((pokemon, index) =>
+                index === optionIndex 
+                    ? { name, pokemonTypes, spriteUrl }
+                    : pokemon
+            )
+        }));
+    };
+    
+
+    const [storageButtonBackgroundImage, setStorageButtonBackgroundImage] = useState(Array(6).fill(''));
+
+    const storageHandleAddButtonClick = (buttonIndex) => {
+        if (selectedPokemon.storage[buttonIndex]) {
+            const spriteUrl = selectedPokemon.storage[buttonIndex].spriteUrl;
+            setStorageButtonBackgroundImage(prevState => ({
+                ...prevState,
+                [buttonIndex]: spriteUrl
+            }));
+        }
+    }
+
+    function StoragePlayer1PokemonSelect() {
+        const input1Ref = useRef(null);
+        const displayFirstName = () => {
+            const input1Value = input1Ref.current.value;
+            setStoragePlayer2Nickname (input1Value);
+        }
+
+    return (
+        <td className={styles.pokemonDisplay}> 
+            <div className={styles.popUpMenu} style={{
+                display: showPopUp[0] ? "flex" : "none", 
+                position: "absolute", 
+                bottom: "65px", 
+                left: "-40px"
+            }}>
+            <div className={styles.selectWrapper}>
+                <Select 
+                    placeholder="Find Pokemon"
+                    onChange={(selectedOption) => {storageHandleSelectChange(player1Index, selectedOption)}}
+                    formatOptionLabel={(pokemon) => {
+                        return ( 
+                            <div style={{ display: 'flex', alignItems: 'center'}}>
+                                <img src={pokemon.spriteUrl} alt={pokemon.name} style={{ width: 50, marginRight: 5 }}></img>
+                                <span>{pokemon.name}</span>
+                            </div>)
+                    }}
+                    options={pokemonInfo} 
+                    getOptionLabel={options => options.name}
+                    getOptionValue={options => options.name}
+                    value={selectedPokemon[player1Index] && pokemonInfo.find(pokemon => pokemon.name === selectedPokemon[player1Index].name)}
+                    />
+            </div>
+            <input 
+                type="text"
+                placeholder='Enter nickname'
+                ref={input1Ref}
+            />
+
+            <div className={styles.popUpMenuBtns}>
+                <button onClick={() => {togglePopUp(0)}}>Cancel</button>
+                <button 
+                onClick={() => {togglePopUp(0); displayFirstName(); storageHandleAddButtonClick(player1Index)}}>Add</button>
+            </div>
+        </div>
+
+        <button style={{  
+            backgroundImage: `url(${storageButtonBackgroundImage[player1Index]})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            height: "4rem", 
+            width: "4rem",
+            borderRadius: '15px',
+            border: 'none',
+        }}
+            onClick={() => {togglePopUp(0)}}>
+        +</button>
+    </td>
+    )
+    }
+
+    function StoragePlayer2PokemonSelect() {
+        const input2Ref = useRef(null);
+        const displaySecondName = () => {
+            const input2Value = input2Ref.current.value;
+            setStoragePlayer2Nickname (input2Value);
+        }
+
+    // console.log(storageButtonBackgroundImage);
+
+    return (
+        <td className={styles.pokemonDisplay}> 
+        <div className={styles.popUpMenu} style={{
+            display: showPopUp[1] ? "flex" : "none", 
+            position: "absolute", 
+            bottom: "65px", 
+            left: "-40px"
+        }}>
+            <div className={styles.selectWrapper}>
+                <Select 
+                    placeholder="Find Pokemon"
+                    onChange={(selectedOption) => {storageHandleSelectChange(player2Index, selectedOption)}}
+                    formatOptionLabel={(pokemon) => {
+                        return ( 
+                            <div style={{ display: 'flex', alignItems: 'center'}}>
+                                <img src={pokemon.spriteUrl} alt={pokemon.name} style={{ width: 50, marginRight: 5 }}></img>
+                                <span>{pokemon.name}</span>
+                            </div>)
+                    }}
+                    options={pokemonInfo} 
+                    getOptionLabel={options => options.name}
+                    getOptionValue={options => options.name}
+                    value={selectedPokemon[player2Index] && pokemonInfo.find(pokemon => pokemon.name === selectedPokemon[player2Index].name)}
+                    />
+            </div>
+            <input 
+                type="text" 
+                placeholder='Enter nickname'
+                ref={input2Ref}
+            />
+
+            <div className={styles.popUpMenuBtns}>
+                <button onClick={() => {togglePopUp(1)}}>Cancel</button>
+                <button 
+                onClick={() => {togglePopUp(1); displaySecondName(); storageHandleAddButtonClick(player2Index)}}>Add</button>
+            </div>
+        </div>
+
+        <button style={{  
+            backgroundImage: `url(${storageButtonBackgroundImage[player2Index]})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            height: "4rem", 
+            width: "4rem",
+            borderRadius: '15px',
+            border: 'none',
+        }}
+            onClick={() => {togglePopUp(1)}}>
+        +</button>
+    </td>
+    )
+    }
+    return (
+        <tr>
+            <td>
+                <input 
+                    type="text" 
+                    placeholder='e.g. route 101' 
+                    style={{
+                    fontSize: '0.9rem'
+                }}></input>
+            </td>
+            <StoragePlayer1PokemonSelect player1Index={0}/>
+            <StoragePlayer2PokemonSelect player2Index={1}/>
+
+            <td className={styles.nicknames}>123</td>
+        </tr>
+
+
+        
+        
+    )
+};
+
+    
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,14 +1,24 @@
-import styles from './Table.module.css'
-import React, { useState } from 'react';
+import styles from './Table.module.css';
+import React, { useState, useEffect } from 'react';
 import TableRow from './TableRow';
 import PokemonStorage from './PokemonStorage';
+import PokemonInfo from './PokemonData';
+
 
 export default function Table() {
     const [player1NameInput, setplayer1NameInput] = useState("");
     const [player2NameInput, setplayer2NameInput] = useState("");
     const [player1Name, setplayer1Name] = useState("");
     const [player2Name, setplayer2Name] = useState("");
+    const [pokemonInfo, setPokemonInfo] = useState([]); 
 
+
+    useEffect(()=>{
+        PokemonInfo()
+            .then((data) => {
+                setPokemonInfo(data)
+            })
+    }, [])
 
     const handleChange = (event) => {
         // extracting name and value properties from event.target which is input
@@ -41,60 +51,51 @@ export default function Table() {
     };
 
     const [selectedPokemon, setSelectedPokemon] = useState({
-        0: {name: '', pokemonTypes: '', spriteUrl: ''},
-        1: {name: '', pokemonTypes: '', spriteUrl: ''},
-        2: {name: '', pokemonTypes: '', spriteUrl: ''},
-        3: {name: '', pokemonTypes: '', spriteUrl: ''},
-        4: {name: '', pokemonTypes: '', spriteUrl: ''},
-        5: {name: '', pokemonTypes: '', spriteUrl: ''},
-        6: {name: '', pokemonTypes: '', spriteUrl: ''},
-        7: {name: '', pokemonTypes: '', spriteUrl: ''},
-        8: {name: '', pokemonTypes: '', spriteUrl: ''},
-        9: {name: '', pokemonTypes: '', spriteUrl: ''},
-        10: {name: '', pokemonTypes: '', spriteUrl: ''},
-        11: {name: '', pokemonTypes: '', spriteUrl: ''},
+        team: Array(12).fill({ name: '', pokemonTypes: '', spriteUrl: ''}),
+        storage: Array(12).fill({ name: '', pokemonTypes: '', spriteUrl: ''})
     });
+    
+    console.log(selectedPokemon)
 
     const handleSelectChange = (optionIndex, selectedOption) => {
         const { name, pokemonTypes, spriteUrl } = selectedOption;
         setSelectedPokemon(prevState => ({
             ...prevState,
-            [optionIndex]: {
-                name: name,
-                pokemonTypes: pokemonTypes,
-                spriteUrl: spriteUrl
-            }
+            team: prevState.team.map((pokemon, index) =>
+                index === optionIndex 
+                    ? { name, pokemonTypes, spriteUrl }
+                    : pokemon
+            )
         }));
+        
     }
 
     const [buttonBackgroundImage, setButtonBackgroundImage] = useState({
-        0: '',
-        1: '',
-        2: '',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        8: '',
-        9: '',
-        10: '',
-        11: '',
+        team: Array(12).fill(''),
+        storage: Array(12).fill('')
     });
+    console.log(buttonBackgroundImage)
 
-    const handleAddButtonClick = (buttonIndex) => {
-        if (selectedPokemon[buttonIndex]) {
-            const spriteUrl = selectedPokemon[buttonIndex].spriteUrl;
+
+    const handleAddButtonClick = (playerIndex) => {
+        if (selectedPokemon.team[playerIndex]) {
+            const spriteUrl = selectedPokemon.team[playerIndex].spriteUrl;
+    
             setButtonBackgroundImage(prevState => ({
                 ...prevState,
-                [buttonIndex]: spriteUrl
+                team: prevState.team.map((image, index) => 
+                    index === playerIndex 
+                        ? spriteUrl 
+                        : image 
+                )
             }));
         }
-    }
+    };
+    
 
     return(
         <div className={styles.tableContainer}>
-            <div className={styles.teamLayout}>
+            <div className={styles.team}>
                 <div className={styles.playersNameInput}>
                     <input
                         type="text"
@@ -141,6 +142,7 @@ export default function Table() {
                             handleSelectChange,
                             handleAddButtonClick
                         }}
+                        pokemonInfo={pokemonInfo}
                     />
 
                     <TableRow
@@ -155,6 +157,7 @@ export default function Table() {
                             handleSelectChange,
                             handleAddButtonClick
                         }}
+                        pokemonInfo={pokemonInfo}
                     />
 
                     <TableRow
@@ -169,6 +172,7 @@ export default function Table() {
                             handleSelectChange,
                             handleAddButtonClick
                         }}
+                        pokemonInfo={pokemonInfo}
                     />
 
                     <TableRow
@@ -183,6 +187,7 @@ export default function Table() {
                             handleSelectChange,
                             handleAddButtonClick
                         }}
+                        pokemonInfo={pokemonInfo}
                     />
 
                     <TableRow
@@ -197,6 +202,7 @@ export default function Table() {
                             handleSelectChange,
                             handleAddButtonClick
                         }}
+                        pokemonInfo={pokemonInfo}
                     />
 
                     <TableRow
@@ -211,15 +217,85 @@ export default function Table() {
                             handleSelectChange,
                             handleAddButtonClick
                         }}
+                        pokemonInfo={pokemonInfo}
                     />            
                     </tbody>
                 </table>
             </div>
 
             <table className={styles.storage}>
+
                 <tbody>
-                        
-                            
+                    <PokemonStorage
+                        playerProps={{
+                            selectedPokemon,
+                            buttonBackgroundImage
+                        }}
+                        selectedProps={{
+                            handleSelectChange,
+                            handleAddButtonClick
+                        }}
+                        pokemonInfo={pokemonInfo}
+                        setSelectedPokemon={setSelectedPokemon}
+                    />
+
+                    <PokemonStorage
+                        playerProps={{
+                            selectedPokemon,
+                            buttonBackgroundImage
+                        }}
+                        selectedProps={{
+                            handleSelectChange,
+                            handleAddButtonClick
+                        }}
+                        pokemonInfo={pokemonInfo}
+                    />
+                    <PokemonStorage
+                        playerProps={{
+                            selectedPokemon,
+                            buttonBackgroundImage
+                        }}
+                        selectedProps={{
+                            handleSelectChange,
+                            handleAddButtonClick
+                        }}
+                        pokemonInfo={pokemonInfo}
+                    />
+
+                    <PokemonStorage
+                        playerProps={{
+                            selectedPokemon,
+                            buttonBackgroundImage
+                        }}
+                        selectedProps={{
+                            handleSelectChange,
+                            handleAddButtonClick
+                        }}
+                        pokemonInfo={pokemonInfo}
+                    />
+                    <PokemonStorage
+                        playerProps={{
+                            selectedPokemon,
+                            buttonBackgroundImage
+                        }}
+                        selectedProps={{
+                            handleSelectChange,
+                            handleAddButtonClick
+                        }}
+                        pokemonInfo={pokemonInfo}
+                    />
+
+                    <PokemonStorage
+                        playerProps={{
+                            selectedPokemon,
+                            buttonBackgroundImage
+                        }}
+                        selectedProps={{
+                            handleSelectChange,
+                            handleAddButtonClick
+                        }}
+                        pokemonInfo={pokemonInfo}
+                    />
                 </tbody>
             </table>
         </div>
