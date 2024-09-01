a570da601ed9d000c371bcfe3bc4d99425187bfd
TackWooLee-Leon <155127100+TackWooLee-Leon@users.noreply.github.com>
2024-09-01 18:21:57 -0400

    fix minor bug

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -92,7 +92,7 @@ export default function Table() {
 
     const teamRenderRows = () => {
         return Array(6).fill().map((_, rowIndex) => {
-            const player1Index = currentPage * 12 + rowIndex * 2;
+            const player1Index = rowIndex * 2;
             const player2Index = player1Index + 1;
                 return (
                     <tr key={rowIndex}>
