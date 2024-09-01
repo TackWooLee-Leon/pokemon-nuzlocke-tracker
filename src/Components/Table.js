6505df334b7f732945b4c84860aa1cfd9e8f28bd
TackWooLee-Leon <155127100+TackWooLee-Leon@users.noreply.github.com>
2024-09-01 18:34:24 -0400

    testing

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -200,6 +200,7 @@ export default function Table() {
                         
                     <tbody>
                         {teamRenderRows()}
+
                     </tbody>
                 </table>
             </div>
